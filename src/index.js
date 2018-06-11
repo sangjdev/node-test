1935f84ca481e7b2c6ea62a6c2918c81cbb6a66a
sangjdev <sangjdev@gmail.com>
2018-06-11 16:47:22 +0900

    포트수정해봄

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
     res.json('success');
 })
 
-app.listen(3000, function () {
-    console.log('express server has started on port 3000');
+app.listen(80, function () {
+    console.log('express server has started on port 80');
 });
